4249a1363532fb281617cb98a39018e27d1e352a
warmanofmars-star <warman.of.mars@gmail.com>
2026-08-29 17:01:42 +0300

    Add GitHub Actions CI/CD

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
+import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.edge.options import Options
 
 from pages.login_page import LoginPage
 from tests.test_login import VALID_PASSWORD, EXISTING_EMAIL
@@ -7,13 +9,27 @@ from tests.test_login import VALID_PASSWORD, EXISTING_EMAIL
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Edge()
-    driver.implicitly_wait(5)
-    driver.maximize_window()
+    options = Options()
 
+    # Проверяем, запускаются ли тесты на сервере GitHub Actions
+    if os.environ.get('CI') == 'true':
+        options.add_argument('--headless')  # Включаем фоновый режим
+        options.add_argument('--no-sandbox')  # Обязательно для Linux-серверов
+        options.add_argument('--disable-dev-shm-usage')  # Обход проблемы с памятью на серверах
+        options.add_argument('--window-size=1920,1080')  # Задаем размер экрана "вслепую"
+
+    # Передаем опции в драйвер
+    driver = webdriver.Edge(options=options)
+
+    # Максимизируем окно только при локальном запуске (с UI)
+    if os.environ.get('CI') != 'true':
+        driver.maximize_window()
+
+    driver.implicitly_wait(5)
 
     yield driver  # Передаем драйвер в тест
     driver.quit()
+
 
 @pytest.fixture
 def authenticated_driver(driver):
@@ -24,7 +40,7 @@ def authenticated_driver(driver):
     login_page.fill_password(VALID_PASSWORD)
     login_page.submit_login()
 
-    # ДОБАВЛЕНО: Ждем, пока авторизация действительно завершится
+    # Ждем, пока авторизация действительно завершится
     login_page.is_logged()
 
     return driver
